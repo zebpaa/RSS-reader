1fe6119cdef231065b245aa9403c9b2aee0e6a54
zebpaa <skvorcov_90@mail.ru>
2024-09-12 14:26:51 +0400

    added locales

---
+++ src/locales/ru.js
@@ -0,0 +1,21 @@
+export default {
+  translation: {
+    // languages: {
+    //   en: 'English',
+    //   ru: 'Русский',
+    // },
+    errors: {
+      invalidForm: 'Ссылка должна быть валидным URL', // 'url must be a valid URL'
+      urlIsNotRSS: 'Ресурс не содержит валидный RSS',
+      rssAlreadyExists: 'RSS уже существует',
+      required: 'Не должно быть пустым',
+      network: 'Ошибка сети',
+    },
+    success: 'RSS успешно загружен',
+    feeds: 'Фиды',
+    posts: {
+      title: 'Посты',
+      button: 'Просмотр',
+    },
+  },
+};
